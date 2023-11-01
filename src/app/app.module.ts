4b101ed1d75dba038b5142a86a1dcafc31dcb4de
SIMONIN Mathieu <simonin.math@gmail.com>
2023-11-01 18:54:33 +0100

    [component]-logo-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './header/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule
