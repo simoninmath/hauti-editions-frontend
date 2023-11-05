5af35d65bda909dd67baa48ab5c9f81667116e34
SIMONIN Mathieu <simonin.math@gmail.com>
2023-11-05 16:26:58 +0100

    [component]-testing-eventbinding

--- src/app/header/search/search.component.ts
+++ src/app/header/search/search.component.ts
@@ -1,11 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
-@Component ({
-    selector:'app-search',
-    templateUrl:'./search.component.html',
-    styleUrls:['./search.component.scss']
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
 })
+export class SearchComponent implements OnInit {
+  allowAdd = false;
+  creationStatus = 'No Add created';
 
-export class SearchComponent {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
+  onAdd() {
+    return;
+  }
+
+  addCreationStatus() {
+    this.creationStatus = 'Add created';
+  }
+
+  onUpdatedAdd(event: any) {
+    console.log(event);
+  }
 }
