3a738bb34113c62c62bc17f3e438b17dba8258bc
SIMONIN Mathieu <simonin.math@gmail.com>
2023-11-06 21:47:06 +0100

    [reset]-reset-all-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { LogoComponent } from './header/logo/logo.component';
-import { SearchComponent } from './header/search/search.component';
 import { FormsModule } from '@angular/forms';
 import { NgModel } from '@angular/forms';
 import { Directive } from '@angular/core';
 
 @NgModule({
   declarations: [
-    AppComponent, 
-    HeaderComponent, 
-    LogoComponent, 
-    SearchComponent
+    AppComponent,
   ],
 
   imports: [
     BrowserModule,
     FormsModule,
-//    Directive
   ],
 
   providers: [],
