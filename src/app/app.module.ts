e654c0368d0a49b1c68612ca60948889fc12b787
SIMONIN Mathieu <simonin.math@gmail.com>
2023-11-08 23:04:15 +0100

    [component]-create-header-search-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,15 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NgModel } from '@angular/forms';
 import { Directive } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './header/search/search.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, SearchComponent],
 
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  imports: [BrowserModule, FormsModule],
 
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-
-}
+export class AppModule {}
